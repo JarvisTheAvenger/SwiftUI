a1cc4714fc51fa12f3fbb40f9ca8cfd7710d4202
Rahul Umap <umaprahul26@gmail.com>
2021-12-13 15:47:24 +0530

    Day 1: @State Property wrappers

---
+++ Day 1/WeSplit-State/WeSplit-State/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit-State
+//
+//  Created by Rahul on 13/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var tapCount = 0
+    
+    var body: some View {
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 1/WeSplit-State/WeSplit-State/WeSplit_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_StateApp.swift
+//  WeSplit-State
+//
+//  Created by Rahul on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
