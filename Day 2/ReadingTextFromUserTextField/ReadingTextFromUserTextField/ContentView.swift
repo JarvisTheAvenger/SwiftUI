f1d4ed833bee611495f0e18ae9a6204c3ed6d100
Rahul Umap <umaprahul26@gmail.com>
2021-12-14 09:37:00 +0530

    - Day 2: PickerStyle, @State, @FocusState, Spacer

---
+++ Day 2/ReadingTextFromUserTextField/ReadingTextFromUserTextField/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  ReadingTextFromUserTextField
+//
+//  Created by Rahul on 14/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var checkAmount = 0.0
+    @State var numberOfPeople = 2
+    @State var tipPercentage = 20
+    
+    @FocusState var isAmountFocused: Bool
+    
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+
+    var body: some View {
+        
+        NavigationView {
+            Form  {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { value in
+                            Text("\(value) %")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isAmountFocused = false
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 2/ReadingTextFromUserTextField/ReadingTextFromUserTextField/ReadingTextFromUserTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingTextFromUserTextFieldApp.swift
+//  ReadingTextFromUserTextField
+//
+//  Created by Rahul on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadingTextFromUserTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
