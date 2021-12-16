2994b0b8840e37d16e5c425abda1f36a2aa7ca47
Rahul Umap <umaprahul26@gmail.com>
2021-12-16 08:44:37 +0530

    Challenge Day

---
+++ Day 4/ChallengeDay/ChallengeDay/ChallengeDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDayApp.swift
+//  ChallengeDay
+//
+//  Created by Rahul on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 4/ChallengeDay/ChallengeDay/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  ChallengeDay
+//
+//  Created by Rahul on 16/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var units = ["meters", "kilometers", "feet", "miles"]
+    
+    @State var inputUnit = "meters"
+    @State var outputUnit = "kilometers"
+    @State var value: Double = 1
+    
+    private func getUnitType(_ unit: String) -> UnitLength {
+        switch unit {
+        case "meters": return .meters
+        case "kilometers": return .kilometers
+        case "feet": return .feet
+        default: return .miles
+        }
+    }
+    
+    var result : Double {
+        guard inputUnit != outputUnit else {
+            return Double(value)
+        }
+            
+        let input = Measurement(value: value, unit: getUnitType(inputUnit))
+        
+        let output = input.converted(to: getUnitType(outputUnit))
+    
+        return output.value
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter value", value: $value, format: .number)
+                        .keyboardType(.numberPad)
+                } header: {
+                    Text("Enter Value")
+                }
+                
+                Section {
+                    Picker("Select Input Unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("From Unit")
+                }
+                
+                Section {
+                    Picker("Selet Output Unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("To Unit")
+                }
+                
+                Section {
+                    Text("\(result)")
+                } header: {
+                    Text("Result")
+                }
+                
+            }
+            .navigationTitle("Distance Converter")
+        }
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
