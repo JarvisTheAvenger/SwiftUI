8b34af423798e63d689bf7437525766d9531e2c1
Rahul Umap <umaprahul26@gmail.com>
2021-12-17 09:44:47 +0530

    - Radial gradients

--- Day 5/Gradients/Gradients/ContentView.swift
+++ Day 5/Gradients/Gradients/ContentView.swift
@@ -12,11 +12,14 @@ struct ContentView: View {
         // Linear gradient goes in one direction
 //        LinearGradient(colors: [.red, .blue], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
         
-        LinearGradient(gradient: Gradient(stops: [
-            Gradient.Stop(color: .white, location: 0.45),
-            Gradient.Stop(color: .black, location: 0.55),
-        ]), startPoint: .top, endPoint: .bottom)
-                
+//        LinearGradient(gradient: Gradient(stops: [
+//            Gradient.Stop(color: .white, location: 0.45),
+//            Gradient.Stop(color: .black, location: 0.55),
+//        ]), startPoint: .top, endPoint: .bottom)
+        
+        
+        RadialGradient(colors: [.red, .blue], center: .center, startRadius: 20, endRadius: 200)
+            .ignoresSafeArea()                
     }
 }
 
