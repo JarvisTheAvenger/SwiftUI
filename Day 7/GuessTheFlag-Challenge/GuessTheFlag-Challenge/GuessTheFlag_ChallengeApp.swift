e97a8e0c53af957eba528b3b2e275276f4eeb151
Rahul Umap <umaprahul26@gmail.com>
2021-12-19 08:29:33 +0530

    GuessTheFlag - Challenge Day

---
+++ Day 7/GuessTheFlag-Challenge/GuessTheFlag-Challenge/GuessTheFlag_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_ChallengeApp.swift
+//  GuessTheFlag-Challenge
+//
+//  Created by Rahul on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
