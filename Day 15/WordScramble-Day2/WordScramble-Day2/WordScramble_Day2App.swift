aa12ec17a025294554729673b7b07fb0a4f84991
Rahul Umap <umaprahul26@gmail.com>
2021-12-27 12:36:31 +0530

    Word scramble game completed

---
+++ Day 15/WordScramble-Day2/WordScramble-Day2/WordScramble_Day2App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_Day2App.swift
+//  WordScramble-Day2
+//
+//  Created by Rahul on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_Day2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
