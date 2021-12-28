07a8afebc3db786551aad0c6ae55d16294f79a1a
Rahul Umap <umaprahul26@gmail.com>
2021-12-28 08:28:54 +0530

    Word scramble challenge day

---
+++ Day 16/Word-Scramble-ChallengeDay/Word-Scramble-ChallengeDay/ContentView.swift
@@ -0,0 +1,161 @@
+//
+//  ContentView.swift
+//  Word-Scramble-ChallengeDay
+//
+//  Created by Rahul on 28/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State var score = 0
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            // Challenge - 1 Add toolbar button
+            .toolbar {
+                ToolbarItem {
+                    HStack(alignment: .center, spacing: 10) {
+                        Text("Score: \(score)")
+                        Button("Start Game", action: startGame)
+                    }
+                }
+            }
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+
+        }
+    }
+
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Challenge - 2 disallow answer which are shorter than 3 letters
+        guard answer.count > 3 else {
+            wordError(title: "Word is shorted", message: "Word needs to be more than 3 characters")
+            return
+        }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        score += 1
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+       
+        newWord = ""
+    }
+    
+    func startGame() {
+        score = 0
+        
+        usedWords = []
+        
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+        
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//MARK: - Helper methods
+extension ContentView {
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        if score > 0 {
+            score -= 1
+        }
+        
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
---
+++ Day 16/Word-Scramble-ChallengeDay/Word-Scramble-ChallengeDay/Word_Scramble_ChallengeDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Scramble_ChallengeDayApp.swift
+//  Word-Scramble-ChallengeDay
+//
+//  Created by Rahul on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_Scramble_ChallengeDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
