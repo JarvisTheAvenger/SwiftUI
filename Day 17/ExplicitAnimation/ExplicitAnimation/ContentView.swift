ce333baeeaa08b9581c633c9d6e2f4a390eca199
Rahul Umap <umaprahul26@gmail.com>
2021-12-29 09:37:44 +0530

    Explicit animation - When we are explicitly asking swiftui to animate without bindings or animation modifier

---
+++ Day 17/ExplicitAnimation/ExplicitAnimation/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  ExplicitAnimation
+//
+//  Created by Rahul on 29/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount  = 0.0
+    var body: some View {
+        Button("Tap me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 17/ExplicitAnimation/ExplicitAnimation/ExplicitAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplicitAnimationApp.swift
+//  ExplicitAnimation
+//
+//  Created by Rahul on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ExplicitAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
