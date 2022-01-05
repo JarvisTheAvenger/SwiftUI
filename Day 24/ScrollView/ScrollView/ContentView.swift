54ef3ca056ca2080fb7dda3c4d777b70d912229b
Rahul Umap <umaprahul26@gmail.com>
2022-01-05 09:49:12 +0530

    All about ScrollView LazyVStack, LazyHStack etc.

---
+++ Day 24/ScrollView/ScrollView/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ScrollView
+//
+//  Created by Rahul on 05/01/22.
+//
+
+import SwiftUI
+
+struct CustomTextView: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(1..<100) {
+                   CustomTextView("\($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 24/ScrollView/ScrollView/ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewApp.swift
+//  ScrollView
+//
+//  Created by Rahul on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
