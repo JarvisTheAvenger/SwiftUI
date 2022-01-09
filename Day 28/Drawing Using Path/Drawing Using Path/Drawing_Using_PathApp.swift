1af4a38f27997180af927620673c2a2161a6d0c0
Rahul Umap <umaprahul26@gmail.com>
2022-01-09 06:06:30 +0530

    Drawing using path

---
+++ Day 28/Drawing Using Path/Drawing Using Path/Drawing_Using_PathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_Using_PathApp.swift
+//  Drawing Using Path
+//
+//  Created by Rahul on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_Using_PathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
