714f0a670806f04a28ccb03ea6e1916950599307
Rahul Umap <umaprahul26@gmail.com>
2022-01-09 06:41:56 +0530

    InsettableShape protocol

--- Day 28/Shape/Shape/ContentView.swift
+++ Day 28/Shape/Shape/ContentView.swift
@@ -21,10 +21,13 @@ struct Triagnle: Shape {
     }
 }
 
-struct Arc: Shape {
+struct Arc: InsettableShape {
+    
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
+    
+    var insetAmount = 0.0
 
     func path(in rect: CGRect) -> Path {
         let adjustmentAngle = Angle.degrees(90)
@@ -33,9 +36,15 @@ struct Arc: Shape {
         let adjustedEnd = endAngle - adjustmentAngle
         
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: adjustedStart, endAngle: adjustedEnd, clockwise: !clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: adjustedStart, endAngle: adjustedEnd, clockwise: !clockwise)
 
         return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
 
@@ -45,9 +54,14 @@ struct ContentView: View {
 //            .stroke(.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
 //            .frame(width: 200, height: 200)
         
-        Arc(startAngle: .zero, endAngle: .degrees(110), clockwise: true)
+//        Arc(startAngle: .zero, endAngle: .degrees(110), clockwise: true)
+//            .stroke(.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+//            .frame(width: 200, height: 200)
+        
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
             .stroke(.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
             .frame(width: 200, height: 200)
+            
     }
 }
 
