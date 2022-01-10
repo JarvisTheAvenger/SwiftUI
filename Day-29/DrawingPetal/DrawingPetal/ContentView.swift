f01ed02865f6e8d36144d079e8d1626072956855
Rahul Umap <umaprahul26@gmail.com>
2022-01-10 20:42:41 +0530

    Drawing Petal - Hacking With Swift

---
+++ Day-29/DrawingPetal/DrawingPetal/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  DrawingPetal
+//
+//  Created by Rahul on 10/01/22.
+//
+
+import SwiftUI
+
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = 0
+
+    // How wide to make each petal
+    var petalWidth: Double = 100
+
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+
+        // now send the main path back
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var petalOffset = 0.0
+    @State private var petalWidth = 100.0
+
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .stroke(.red, lineWidth: 1)
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day-29/DrawingPetal/DrawingPetal/DrawingPetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingPetalApp.swift
+//  DrawingPetal
+//
+//  Created by Rahul on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
