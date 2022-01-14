2436e7f00428bb2e436656dee8f8de9cc0045b15
Rahul Umap <umaprahul26@gmail.com>
2022-01-14 09:24:46 +0530

    Async-Await usage in SwiftUI

---
+++ Day 31/URLSession-Codable/URLSession-Codable/URLSession_CodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_CodableApp.swift
+//  URLSession-Codable
+//
+//  Created by Rahul on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_CodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
