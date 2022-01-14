186793b713d1df5830feca137c7fe3aecb8a97b1
Rahul Umap <umaprahul26@gmail.com>
2022-01-14 09:24:19 +0530

    Async Image usage

---
+++ Day 31/AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Rahul on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 31/AsyncImage/AsyncImage/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Rahul on 14/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
