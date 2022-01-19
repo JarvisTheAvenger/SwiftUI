0b8e1b2bf92dfbefb765e25e3662dbd08e55f287
Rahul Umap <umaprahul26@gmail.com>
2022-01-19 10:42:03 +0530

    Removed Z stack

--- Day 35 - Day 36/Bookworm/Bookworm/DetailView.swift
+++ Day 35 - Day 36/Bookworm/Bookworm/DetailView.swift
@@ -17,21 +17,6 @@ struct DetailView: View {
 
     var body: some View {
         ScrollView {
-            ZStack(alignment: .bottomTrailing) {
-                Image(book.genre ?? "Fantasy")
-                    .resizable()
-                    .scaledToFit()
-                
-                Text(book.genre?.uppercased() ?? "FANTASY")
-                    .font(.caption)
-                    .fontWeight(.black)
-                    .padding(8)
-                    .foregroundColor(.white)
-                    .background(.black.opacity(0.75))
-                    .clipShape(Capsule())
-                    .offset(x: -5, y: -5)
-            }
-            
             
             Text(book.author ?? "Unknown author")
                 .font(.title)
@@ -42,6 +27,7 @@ struct DetailView: View {
 
             RatingView(rating: .constant(Int(book.rating)))
                 .font(.largeTitle)
+
         }
         .alert("Delete book", isPresented: $showingDeleteAlert) {
             Button("Delete", role: .destructive, action: deleteBook)
