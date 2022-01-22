a47ed54ab83ce63728eca86e51e91babb85416fe
Rahul Umap <umaprahul26@gmail.com>
2022-01-22 19:29:48 +0530

    NSPredicate - filtering

---
+++ Day 40/CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Rahul on 22/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
+    
+    var body: some View {
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
+            }
+            .padding()
+        }
+    }
+}
+
---
+++ Day 40/CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,21 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Rahul on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    @StateObject private var dataController = DataController()
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
