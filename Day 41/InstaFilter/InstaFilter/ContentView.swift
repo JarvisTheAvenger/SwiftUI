436f7529e03030eb71324ee50e849f397f89255f
Rahul Umap <umaprahul26@gmail.com>
2022-01-31 10:19:08 +0530

    Property wrapper

---
+++ Day 41/InstaFilter/InstaFilter/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by Rahul on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+
+            Slider(value: $blurAmount, in: 0...20)
+
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 41/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Rahul on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
