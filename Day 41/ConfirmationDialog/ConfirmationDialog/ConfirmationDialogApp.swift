afd4f6028b36716bb8b78f002b2bb9ceb88fe42f
Rahul Umap <umaprahul26@gmail.com>
2022-01-31 10:24:33 +0530

    Confirmation dialog

---
+++ Day 41/ConfirmationDialog/ConfirmationDialog/ConfirmationDialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfirmationDialogApp.swift
+//  ConfirmationDialog
+//
+//  Created by Rahul on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ConfirmationDialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 41/ConfirmationDialog/ConfirmationDialog/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  ConfirmationDialog
+//
+//  Created by Rahul on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
+            }
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
+            }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
