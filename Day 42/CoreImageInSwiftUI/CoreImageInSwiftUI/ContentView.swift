66c684a41799a0fe461df330da6bc7856b087f41
Rahul Umap <umaprahul26@gmail.com>
2022-02-01 09:31:31 +0530

    Core image in SwiftUI

---
+++ Day 42/CoreImageInSwiftUI/CoreImageInSwiftUI/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  CoreImageInSwiftUI
+//
+//  Created by Rahul on 01/02/22.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+
+struct ContentView: View {
+    @State private var image: Image?
+
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+            let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+
+            // and convert that to a SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 42/CoreImageInSwiftUI/CoreImageInSwiftUI/CoreImageInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageInSwiftUIApp.swift
+//  CoreImageInSwiftUI
+//
+//  Created by Rahul on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
