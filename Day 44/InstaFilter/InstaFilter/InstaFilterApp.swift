4533c1bd4dab048380a663e541e540efd2e56e46
Rahul Umap <umaprahul26@gmail.com>
2022-02-02 10:15:47 +0530

    InstaFilter - with the challenge and review.

---
+++ Day 44/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Rahul on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
