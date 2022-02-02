437aa2f9b3426131cc95e66171b3af2b8dbe61d6
Rahul Umap <umaprahul26@gmail.com>
2022-02-02 10:28:31 +0530

    Touch/Face id integration

---
+++ Day 47 - Day 48/TouchIDIntegration/TouchIDIntegration/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  TouchIDIntegration
+//
+//  Created by Rahul on 02/02/22.
+//
+
+import SwiftUI
+import LocalAuthentication
+
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+
+    
+    var body: some View {
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 47 - Day 48/TouchIDIntegration/TouchIDIntegration/TouchIDIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchIDIntegrationApp.swift
+//  TouchIDIntegration
+//
+//  Created by Rahul on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchIDIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
