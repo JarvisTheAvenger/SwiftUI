099cc6c9c84aa96e668635ba7efc8a1bc2251ca9
Rahul Umap <umaprahul26@gmail.com>
2022-02-02 10:25:05 +0530

    - Map kit integration

---
+++ Day 47 - Day 48/MapKitIntegration/MapKitIntegration/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  MapKitIntegration
+//
+//  Created by Rahul on 02/02/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct ContentView: View {
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+
+    
+    var body: some View {
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 47 - Day 48/MapKitIntegration/MapKitIntegration/MapKitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitIntegrationApp.swift
+//  MapKitIntegration
+//
+//  Created by Rahul on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MapKitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
